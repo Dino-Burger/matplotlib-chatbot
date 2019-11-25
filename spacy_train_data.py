ddb62b861ea9b9e5f5069e74fcef3ba954d31fc8
Dino Burger <dinohere@gmail.com>
2019-11-25 13:06:37 +0100

    cleanup of code to fill variables

--- spacy_train_data.py
+++ spacy_train_data.py
@@ -20,50 +20,52 @@ variable_train_values = {
 }
 
 
+def fill_examples_variables(sentences, variable_train_values):
+    import itertools
+    import copy
+    import numpy as np
+
+    tokenized_sentences = [ t.split() for t in sentences ]
+
+    def get_vars(str_list):
+        return [(i,x) for i,x in enumerate(str_list) if x[0]=='$']
+
+    # all tokenized sentences with a tuple of all variables it contains
+    tok_sent_vars = [ (t,get_vars(t)) for t in tokenized_sentences ]
+
+    # replace the variable names (e.g. $color) in the tokenized sentence 
+    # with values (e.g. red)
+    exploded_sentences = []
+    for tok_sent, vars_ind_val in tok_sent_vars:
+        ti_vars = [ variable_train_values[ti_var] for ti_i, ti_var in vars_ind_val ]
+        ti_is = [ ti_i for ti_i, ti_var in vars_ind_val ]
+        for element in itertools.product(*ti_vars):
+            #print(tok_sent, ti_is, element)
+            new_t = copy.deepcopy(tok_sent)
+            for overwrite_index, overwrite_string in zip(ti_is, element):
+                new_t[overwrite_index] = overwrite_string
+            exploded_sentences.append((new_t, vars_ind_val))
 
 
-ts = [ "my $color and your $color and the other $color",
-        "plot column $columnname from variable $variable" ]
-tss = [ t.split() for t in ts ]
+    # join sentences to astring together and output the indexes 
+    # where each variable is sitting
+    joined_sentences = []
+    for tokenized_sentence, variable_list in exploded_sentences:
+        # the +1 is for the spaces we insert
+        token_lenghts = [ len(ts)+1 for ts in tokenized_sentence ]
+        token_cum_lengths = [0,] + list(np.cumsum(token_lenghts))
+        
+        sentence = ' '.join(tokenized_sentence)
+        # the -1 is to ignore the trailing space
+        containded_tokens = [ (token_cum_lengths[var_index], 
+                                token_cum_lengths[var_index+1]-1, 
+                                var_name) 
+                                for var_index, var_name in variable_list ]
 
-def get_vars(str_list):
-    return [(i,x) for i,x in enumerate(str_list) if x[0]=='$']
-
-tsi = [ (t,get_vars(t)) for t in tss ]
-
-import itertools
-import copy
-import numpy as np
-
-result = []
-for t, ti in tsi:
-    ti_vars = [ variable_train_values[ti_var] for ti_i, ti_var in ti ]
-    ti_is = [ ti_i for ti_i, ti_var in ti ]
-    for element in itertools.product(*ti_vars):
-        #print(t, ti_is, element)
-        new_t = copy.deepcopy(t)
-        for overwrite_index, overwrite_string in zip(ti_is, element):
-            new_t[overwrite_index] = overwrite_string
-        result.append((new_t, ti))
+        joined_sentences.append((sentence, containded_tokens))
 
 
-result2 = []
-for tokenized_sentence, variable_list in result:
-    # the +1 is for the spaces we insert
-    token_lenghts = [ len(ts)+1 for ts in tokenized_sentence ]
-    token_cum_lengths = [0,] + list(np.cumsum(token_lenghts))
-    
-    sentence = ' '.join(tokenized_sentence)
-    # the -1 is to ignore the trailing space
-    containded_tokens = [ (token_cum_lengths[var_index], 
-                            token_cum_lengths[var_index+1]-1, 
-                            var_name) 
-                            for var_index, var_name in variable_list ]
-
-    result2.append((sentence, containded_tokens))
-
-
-
+    return joined_sentences
 
 
 
