2e0db2c5327783da57af1230cd7c4b1a48aee4d5
Dino Burger <dinohere@gmail.com>
2019-11-28 22:38:09 +0100

    further class-ifying chatbot

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import importlib
 
-conf = importlib.import_module('chatbot_config')
+
 
 def process_input_data(input_data):
     # replace ["*"] in start_states by actual list of all states
@@ -12,52 +12,12 @@ def process_input_data(input_data):
             member["start_states"] = all_intents
     return input_data
 
-input_data = process_input_data(conf.input_data_raw)
-input_data_edges = [ member for member in input_data if "start_states" in member]
-input_data_nodes = [ member for member in input_data if "intent" in member]
 
 
-# cosine tfidf model
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
-word_vectorizer = TfidfVectorizer()
-all_patterns = [pat for edge in input_data_edges for pat in edge["patterns"]]
-word_vectorizer.fit(all_patterns)
 
-# add tfidf vectors to input_data_edges
-for edge in input_data_edges:
-    patterns = edge["patterns"]
-    pattern_vectors = word_vectorizer.transform(patterns)
-    edge["pattern_vectors"] = pattern_vectors
 
-def get_possible_next_pattern_vectors_old(curr_state):
-    # returns [(pat_vec, pat, end_state)]
-    next_states = [ (member["pattern_vectors"][i_vec], 
-                    member["patterns"][i_vec],
-                    member["end_state"]) 
-                    for member in input_data_edges 
-                    for i_vec in range(member["pattern_vectors"].shape[0])
-                    if curr_state in member["start_states"]]
-    return next_states
 
-def get_possible_next_pattern_vectors(curr_state, curr_contexts):
-    # returns [(pat_vec, pat, end_state)]
-    next_states = [ (edge["pattern_vectors"][i_vec], 
-                    edge["patterns"][i_vec],
-                    edge["end_state"]) 
-                    for edge in input_data_edges 
-                    for i_vec in range(edge["pattern_vectors"].shape[0])
-                    if curr_state in edge["start_states"]
-                    and set(get_context_require_from_intent(edge["end_state"])).issubset(set(curr_contexts))]
-    return next_states
-
-def get_closest_command(possible_next_pattern_vectors: list, inp:str):
-    input_vector = word_vectorizer.transform([inp])
-    all_distances = [(cosine_similarity(input_vector, pat_vec)[0][0], pat, end_state)
-                        for pat_vec, pat, end_state in possible_next_pattern_vectors ]
-    max_command = max(all_distances, key = lambda l: l[0])
-    return max_command
 
 def get_response_from_intent(intent):
     response = [ node.get("response", "") for node in input_data_nodes
@@ -77,19 +37,61 @@ def get_context_set_from_intent(intent):
     assert(len(response)==1)
     return response[0]
 
-def get_field_from_intent(field_name, intent, default=[]):
-    response = [ node.get(field_name, default) for node in input_data_nodes
-                    if node['intent'] == intent]
-    assert(len(response)==1)
-    return response[0]
+
 
 
 
 
 
 class Chatbot:
-    def __init__(self,local_vars):
+    def __init__(self, local_vars, config_file = 'chatbot_config'):
         self.local_vars = local_vars
+
+        self.conf = importlib.import_module(config_file)
+
+        self.input_data = process_input_data(self.conf.input_data_raw)
+        self.input_data_edges = [ member for member in self.input_data if "start_states" in member]
+        self.input_data_nodes = [ member for member in self.input_data if "intent" in member]
+
+
+        # cosine tfidf model
+        from sklearn.feature_extraction.text import TfidfVectorizer
+
+        self.word_vectorizer = TfidfVectorizer()
+        self.all_patterns = [pat for edge in self.input_data_edges for pat in edge["patterns"]]
+        self.word_vectorizer.fit(self.all_patterns)
+
+        # add tfidf vectors to input_data_edges
+        for edge in self.input_data_edges:
+            patterns = edge["patterns"]
+            pattern_vectors = self.word_vectorizer.transform(patterns)
+            edge["pattern_vectors"] = pattern_vectors
+
+    def get_possible_next_pattern_vectors(self, curr_state, curr_contexts):
+        # returns [(pat_vec, pat, end_state)]
+        next_states = [ (edge["pattern_vectors"][i_vec], 
+                        edge["patterns"][i_vec],
+                        edge["end_state"]) 
+                        for edge in self.input_data_edges 
+                        for i_vec in range(edge["pattern_vectors"].shape[0])
+                        if curr_state in edge["start_states"]
+                        and set(get_context_require_from_intent(edge["end_state"])).issubset(set(curr_contexts))]
+        return next_states
+
+    def get_closest_command(self, possible_next_pattern_vectors: list, inp:str):
+        from sklearn.metrics.pairwise import cosine_similarity
+        input_vector = self.word_vectorizer.transform([inp])
+        all_distances = [(cosine_similarity(input_vector, pat_vec)[0][0], pat, end_state)
+                            for pat_vec, pat, end_state in possible_next_pattern_vectors ]
+        max_command = max(all_distances, key = lambda l: l[0])
+        return max_command
+
+    def get_field_from_intent(self, field_name, intent, default=[]):
+        response = [ node.get(field_name, default) for node in self.input_data_nodes
+                        if node['intent'] == intent]
+        assert(len(response)==1)
+        return response[0]
+
     def run(self):
         from matplotlib import pyplot as plt
         curr_state = "entry"
@@ -112,12 +114,12 @@ class Chatbot:
             print("current Contexts", curr_contexts)
 
             # possible_next_pattern_vectors = get_possible_next_pattern_vectors_old(curr_state)
-            possible_next_pattern_vectors = get_possible_next_pattern_vectors(curr_state, curr_contexts)
+            possible_next_pattern_vectors = self.get_possible_next_pattern_vectors(curr_state, curr_contexts)
             possible_next_states = list(set([ns for pat_vec, pat, ns in possible_next_pattern_vectors]))
             print(possible_next_states)
 
             inp = input()
-            rating, pat, next_state = get_closest_command(possible_next_pattern_vectors, inp)
+            rating, pat, next_state = self.get_closest_command(possible_next_pattern_vectors, inp)
             required_contexts = get_context_require_from_intent(next_state)
 
             if inp == 'end':
@@ -131,7 +133,7 @@ class Chatbot:
                 print("sry, you lack context", lacking_context, "to do this")
                 continue
 
-            parser = get_field_from_intent("code_command",
+            parser = self.get_field_from_intent("code_command",
                                             next_state, 
                                             default=lambda all_variables, inp, local_vars: all_variables)
             all_variables = parser(all_variables, inp, self.local_vars)
@@ -169,5 +171,16 @@ def get_possible_next_states(curr_state):
 def get_possible_next_patterns(curr_state):
     edges = [ member for member in input_data if "start_states" in member]
     next_states = [ (patt, member["end_state"]) for member in edges for patt in member["patterns"]
+                    if curr_state in member["start_states"]]
+    return next_states
+
+
+def get_possible_next_pattern_vectors_old(curr_state):
+    # returns [(pat_vec, pat, end_state)]
+    next_states = [ (member["pattern_vectors"][i_vec], 
+                    member["patterns"][i_vec],
+                    member["end_state"]) 
+                    for member in input_data_edges 
+                    for i_vec in range(member["pattern_vectors"].shape[0])
                     if curr_state in member["start_states"]]
     return next_states
