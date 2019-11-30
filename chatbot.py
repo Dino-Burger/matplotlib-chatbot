7d8f7de689d77b1888578f49de7fc839783d319c
Dino Burger <dinohere@gmail.com>
2019-11-30 01:13:08 +0100

    updated requirements plus minor tweaks

--- chatbot.py
+++ chatbot.py
@@ -99,7 +99,7 @@ class Chatbot:
             print("Things to do:", ', '.join(possible_things_to_do))
 
             inp = input('> ')
-            if inp == "vv":
+            if inp == "":
 
                 import speech_recognition as sr
 
@@ -115,6 +115,7 @@ class Chatbot:
                     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
                     # instead of `r.recognize_google(audio)`
                     inp = r.recognize_google(audio)
+                    print(inp)
                 except sr.UnknownValueError:
                     print("Google Speech Recognition could not understand audio")
                 except sr.RequestError as e:
