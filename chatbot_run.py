e05b7a8f3fadcfc9fb0d383b367ee3b581d1eaa4
Dino Burger <dinohere@gmail.com>
2019-12-14 13:33:18 +0100

    integrated choice of backend into normal chatbot_run.py file

--- chatbot_run.py
+++ chatbot_run.py
@@ -1,14 +1,31 @@
 import pandas as pd
 import numpy as np
+import matplotlib
+import click
+
 from chatbot import Chatbot
 
-cb = Chatbot(locals())
 
-# some variables
-x= [1,2,4,5,6]
-y= [1,1,1,1,2,3,4,4,4,4,4,4,4,5]
-height = [1,1,1,2,2,]
-df = pd.DataFrame({'a':[1,2,3], 'b':[4,5,6]})
-nn = np.random.normal(size=(2,3))
+@click.command()
+@click.option('--backend', 
+                default=None, 
+                help='Matplotlib backend. e.g. TkAgg')
+def helloBot(backend):
+    if backend:
+        matplotlib.use(backend)
+    print("Matplotlib backend is", matplotlib.get_backend())
 
-cb.run()
+    cb = Chatbot(locals())
+
+    # some variables
+    x= [1,2,4,5,6]
+    y= [1,1,1,1,2,3,4,4,4,4,4,4,4,5]
+    height = [1,1,1,2,2,]
+    df = pd.DataFrame({'a':[1,2,3], 'b':[4,5,6]})
+    nn = np.random.normal(size=(2,3))
+
+    cb.run()
+
+
+if __name__ == '__main__':
+    helloBot()
