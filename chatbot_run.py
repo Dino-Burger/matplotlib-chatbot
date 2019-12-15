9a064389b06d146ac82fde100e6092278d0adfd3
Dino Burger <dinohere@gmail.com>
2019-12-15 11:40:53 +0100

    replaced click with argparse

--- chatbot_run.py
+++ chatbot_run.py
@@ -1,31 +1,27 @@
 import pandas as pd
 import numpy as np
 import matplotlib
-import click
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--backend", help="Matplotlib backend. e.g. TkAgg", default=None)
+args = parser.parse_args()
 
 from chatbot import Chatbot
 
-
-@click.command()
-@click.option('--backend', 
-                default=None, 
-                help='Matplotlib backend. e.g. TkAgg')
-def helloBot(backend):
-    if backend:
-        matplotlib.use(backend)
-    print("Matplotlib backend is", matplotlib.get_backend())
-
-    cb = Chatbot(locals())
-
-    # some variables
-    x= [1,2,4,5,6]
-    y= [1,1,1,1,2,3,4,4,4,4,4,4,4,5]
-    height = [1,1,1,2,2,]
-    df = pd.DataFrame({'a':[1,2,3], 'b':[4,5,6]})
-    nn = np.random.normal(size=(2,3))
-
-    cb.run()
+if args.backend:
+    matplotlib.use(args.backend)
+print("Matplotlib backend is", matplotlib.get_backend())
 
 
-if __name__ == '__main__':
-    helloBot()
+cb = Chatbot(locals())
+
+# some variables
+x= [1,2,4,5,6]
+y= [1,1,1,1,2,3,4,4,4,4,4,4,4,5]
+height = [1,1,1,2,2,]
+df = pd.DataFrame({'a':[1,2,3], 'b':[4,5,6]})
+nn = np.random.normal(size=(2,3))
+
+cb.run()
+
